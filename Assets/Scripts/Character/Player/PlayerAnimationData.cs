a2ff8ac7d5fc7c1cb5e415a7e2e9eaf22fc10231
francJi <minkyuji95@hanmail.net>
2023-10-16 00:25:06 +0900

    [ADD] Attack에 관한 AnimationData 추가

--- Assets/Scripts/Character/Player/PlayerAnimationData.cs
+++ Assets/Scripts/Character/Player/PlayerAnimationData.cs
@@ -10,11 +10,13 @@ public class PlayerAnimationData
     [SerializeField] private string IdleParameterName = "Idle";
     [SerializeField] private string WalkParameterName = "Walk";
     [SerializeField] private string RunParameterName = "Run";
+    [SerializeField] private string AtttackParameterName = "Attack";
 
     public int GroundParameterHash {  get; private set; }
     public int IdleParameterHash { get; private set; }
     public int WalkParameterHash { get; private set; }
     public int RunParameterHash { get; private set; }
+    public int AttackParameterHash { get; private set; }
 
     public void Initialize()
     {
@@ -22,5 +24,6 @@ public class PlayerAnimationData
         IdleParameterHash = Animator.StringToHash(IdleParameterName);
         WalkParameterHash = Animator.StringToHash(WalkParameterName);
         RunParameterHash = Animator.StringToHash(RunParameterName);
+        AttackParameterHash = Animator.StringToHash(AtttackParameterName);
     }
 }
