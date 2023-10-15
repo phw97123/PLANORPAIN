f54cc49856312f6166c6a8e6fa7b2f9d55922b19
장성림 <hoobhoob214@gmail.com>
2023-10-15 22:42:04 +0900

    [FEAT] 자동차 움직이는 기능 추가 (-성림)

--- Assets/Scripts/Vehicle/VehicleController.cs
+++ Assets/Scripts/Vehicle/VehicleController.cs
@@ -6,8 +6,18 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class VehicleController : MonoBehaviour
 {
+    [SerializeField] private WheelCollider[] _wheelCols = new WheelCollider[4];
+    [SerializeField] private GameObject[] _wheels = new GameObject[4];
 
     private Vector2 _curDriveInput;
+
+    private void Start()
+    {
+        for(int i = 0; i < _wheelCols.Length; i++)
+        {
+            _wheelCols[i].transform.position = _wheels[i].transform.position;
+        }
+    }
 
     private void Update()
     {
@@ -16,7 +26,22 @@ public class VehicleController : MonoBehaviour
 
     private void Move()
     {
-        
+        for(int i = 0; i < _wheelCols.Length; i++)
+        {
+            Quaternion quaternion;
+            Vector3 position;
+            _wheelCols[i].GetWorldPose(out position, out quaternion);
+            _wheels[i].transform.SetPositionAndRotation(position, quaternion);
+        }
+
+        _wheelCols[0].steerAngle = _curDriveInput.x * 45f;
+        _wheelCols[2].steerAngle = _curDriveInput.x * 45f;
+
+        for (int i = 0; i < _wheelCols.Length; i++)
+        {
+            _wheelCols[i].motorTorque = _curDriveInput.y * (400f);
+        }
+
     }
 
     public void OnDriveInput(InputAction.CallbackContext context)
