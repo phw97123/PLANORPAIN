380efc9a883d106b4d55770ec78f3727dbf837e3
phw97123 <phw97123@naver.com>
2023-10-15 20:41:41 +0900

    [REFACTOR] 안 쓰는 코드 삭제

--- Assets/Scripts/CleanMiniGame/PlaceObjectOnGrid.cs
+++ Assets/Scripts/CleanMiniGame/PlaceObjectOnGrid.cs
@@ -11,7 +11,7 @@ public class PlaceObjectOnGrid : MonoBehaviour
 
     [SerializeField] private int height;
     [SerializeField] private int width;
-    private Node[,] nodes;
+
 
     void Start()
     {
@@ -20,36 +20,20 @@ public class PlaceObjectOnGrid : MonoBehaviour
 
     private void CreateGrid()
     {
-        nodes = new Node[width, height];
         var name = 0;
-        Vector3 cellSize = gridCellPrefab.transform.localScale; 
+        Vector3 cellSize = gridCellPrefab.transform.localScale;
 
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                Vector3 worldPosition = new Vector3(i *cellSize.x*2f, 0, j*cellSize.z * 2f);
-                Transform obj = Instantiate(gridCellPrefab, worldPosition*2, Quaternion.identity);
+                Vector3 worldPosition = new Vector3(i * cellSize.x * 2f, 0, j * cellSize.z * 2f);
+                Transform obj = Instantiate(gridCellPrefab, worldPosition * 2, Quaternion.identity);
 
                 obj.name = "Cell" + name;
-                nodes[i, j] = new Node(true, worldPosition, obj);
                 name++;
             }
         }
-    }
-}
-
-public class Node
-{
-    public bool isPlaceable;
-    public Vector3 cellPosition;
-    public Transform obj;
-
-    public Node(bool isPlaceable, Vector3 cellPosition, Transform obj)
-    {
-        this.isPlaceable = isPlaceable;
-        this.cellPosition = cellPosition;
-        this.obj = obj;
     }
 }
 
