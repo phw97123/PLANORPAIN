37ab26e3804a20f0c5de591dcb3b8fb27a412d4f
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-10-16 15:35:17 +0900

    [UPDATE] Node 활성화 및 순서 적용 로직 작성

---
+++ Assets/Scripts/DevelopGameScene/Node.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private const int INACTIVE = 0;
+    private const int ACTIVE = 1;
+
+    [SerializeField] private GameObject _interactPanelUI;
+    [SerializeField] private bool _isActive;
+    [SerializeField] private Material[] matList;
+
+    private int _nodeNum;
+    private MeshRenderer _meshRenderer;
+
+    private Graph _graph;
+
+    private void Awake()
+    {
+        _nodeNum = int.Parse((name[name.Length - 1]).ToString());
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _graph = GetComponentInParent<Graph>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // TODO "Player" Tag 추가 후 Tag 비교로 변경
+        if (other.gameObject.name == "Player" && !_isActive)
+        {
+            if (_graph.SearchNode(_nodeNum))
+            {
+                SetNodeActive(true);
+            }
+        }
+    }
+
+    public void SetNodeActive(bool isActive)
+    {
+        _isActive = isActive;
+        if (isActive) _meshRenderer.material = matList[ACTIVE];
+        else _meshRenderer.material = matList[INACTIVE];
+    }
+}
