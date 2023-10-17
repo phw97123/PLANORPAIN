fade4249068330ff22e0aedfa9e7b4f1ff7d4ca5
phw97123 <phw97123@naver.com>
2023-10-17 23:23:43 +0900

    [FEAT] 청소중 상태 추가

--- Assets/Scripts/Character/Player/PlayerAnimationData.cs
+++ Assets/Scripts/Character/Player/PlayerAnimationData.cs
@@ -9,14 +9,16 @@ public class PlayerAnimationData
     [SerializeField] private string groundParameterName = "@Ground";
     [SerializeField] private string IdleParameterName = "Idle";
     [SerializeField] private string WalkParameterName = "Walk";
+    [SerializeField] private string CleaningParameterName = "Cleaning";
     [SerializeField] private string RunParameterName = "Run";
     [SerializeField] private string AtttackParameterName = "@Attack";
     [SerializeField] private string AttackTypeParameterName = "AttackType";
     [SerializeField] private string comboAttackParameterName = "ComboAttack";
 
-    public int GroundParameterHash {  get; private set; }
+    public int GroundParameterHash { get; private set; }
     public int IdleParameterHash { get; private set; }
     public int WalkParameterHash { get; private set; }
+    public int CleaningParameterHash { get; private set; }
     public int RunParameterHash { get; private set; }
     public int AttackParameterHash { get; private set; }
     public int AttackTypeParameterHash { get; private set; }
@@ -24,10 +26,11 @@ public class PlayerAnimationData
 
     public void Initialize()
     {
-        GroundParameterHash = Animator.StringToHash(groundParameterName); 
+        GroundParameterHash = Animator.StringToHash(groundParameterName);
         IdleParameterHash = Animator.StringToHash(IdleParameterName);
-        
+
         WalkParameterHash = Animator.StringToHash(WalkParameterName);
+        CleaningParameterHash = Animator.StringToHash(CleaningParameterName);
         RunParameterHash = Animator.StringToHash(RunParameterName);
 
         AttackParameterHash = Animator.StringToHash(AtttackParameterName);
