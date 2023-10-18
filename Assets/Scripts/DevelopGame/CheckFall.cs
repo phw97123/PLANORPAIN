ca4ed38a6cbd940ab8dde8f897b1ff1d33cc4a3a
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-10-19 05:02:23 +0900

    [UPDATE] 맵 구조물 추가

--- Assets/Scripts/DevelopGame/CheckFall.cs
+++ Assets/Scripts/DevelopGame/CheckFall.cs
@@ -6,15 +6,9 @@ public class CheckFall : MonoBehaviour
 {
     private DevelopGameManager _developGameManager;
 
-    private void Awake()
-    {
-        // 게임 매니저를 통해서 미니 게임을 관리할 매니저 호출
-        
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag(Tags.PLAYER))
         {
             if (_developGameManager == null) _developGameManager = GameManager.Instance.GetMiniGameManager<DevelopGameManager>();
             _developGameManager.Respawn();
