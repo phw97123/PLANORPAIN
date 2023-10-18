3df4af98fa6bbe1914a23c5f89cdbfc2f0683a4d
phw97123 <phw97123@naver.com>
2023-10-18 16:44:20 +0900

    [FEAT] Player InputSystem 분리

--- Assets/Scripts/Character/Player/PlayerInput.cs
+++ Assets/Scripts/Character/Player/PlayerInput.cs
@@ -8,16 +8,12 @@ public class PlayerInput : MonoBehaviour
     public PlayerInputActionsTest InputActions { get; private set; } // Test 버전 적용
     public PlayerInputActionsTest.PlayerActions PlayerActions { get; private set; } // Test 버전 적용
 
-    public CinemachineVirtualCamera virtualCamera;
-
-    public float zoomSpeed = 5.0f;
-    public float minFov = 20.0f;
-    public float maxFov = 50.0f; 
-
     private void Awake()
     {
         InputActions = new PlayerInputActionsTest(); // Test 버전 적용
         PlayerActions = InputActions.Player;
+
+        OnEnable(); 
     }
 
     private void OnEnable()
@@ -25,19 +21,8 @@ public class PlayerInput : MonoBehaviour
         InputActions.Enable();
     }
 
-    private void OnDisable()
+    public void OnDisable()
     {
         InputActions.Disable();
-    }
-
-    private void Update()
-    {
-        float scrollInput = InputActions.Player.CameraZoom.ReadValue<float>();
-
-        if (virtualCamera != null && scrollInput != 0)
-        {
-            virtualCamera.m_Lens.FieldOfView += scrollInput * zoomSpeed; 
-            virtualCamera.m_Lens.FieldOfView = Mathf.Clamp(virtualCamera.m_Lens.FieldOfView,minFov, maxFov);
-        }
     }
 }
